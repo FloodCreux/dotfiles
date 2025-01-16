3ec513ba68fcaff1ff0eb23ea18887d54eab812e
Mike Flood <flood.mike@gmail.com>
2025-01-15 20:13:58 -0600

    more plugins for nvim

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -6,7 +6,7 @@ vim.wo.number = true
 vim.o.relativenumber = true
 
 -- Disable mouse mode
-vim.o.mouse = ""
+vim.o.mouse = "a"
 
 -- Enable break indent
 vim.o.breakindent = true
---
+++ nvim/lua/plugins/folke/init.lua
@@ -0,0 +1,65 @@
+return {
+  require("plugins.folke.snacks"),
+  "folke/zen-mode.nvim",
+  {
+    "folke/trouble.nvim",
+    lazy = false,
+    dependencies = "nvim-tree/nvim-web-devicons",
+    config = function()
+      require("trouble").setup({
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      })
+    end,
+  },
+
+  {
+    "folke/todo-comments.nvim",
+    dependencies = "nvim-lua/plenary.nvim",
+    config = function()
+      require("todo-comments").setup({})
+    end,
+  },
+
+  {
+    "folke/noice.nvim",
+    config = function()
+      require("noice").setup({
+        -- add any options here
+        routes = {
+          {
+            filter = {
+              event = "msg_show",
+              any = {
+                { find = "%d+L, %d+B" },
+                { find = "; after #%d+" },
+                { find = "; before #%d+" },
+                { find = "%d fewer lines" },
+                { find = "%d more lines" },
+              },
+            },
+            opts = { skip = true },
+          },
+        },
+      })
+    end,
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    },
+  },
+  {
+    "folke/twilight.nvim",
+    ft = "markdown",
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  },
+  {
+    "folke/which-key.nvim",
+  },
+}
