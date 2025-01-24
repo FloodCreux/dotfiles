c787bb1767e122d2a915c0dc4143aa5221239fdf
Mike Flood <flood.mike@gmail.com>
2025-01-23 19:46:35 -0600

    sketchybar

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 stow .
 chmod +x ~/.config/tmux/scripts/tmux-sessionizer
+chmod +x ~/.config/sketchybar/plugins/aerospacer.sh
+# Add Logic to build and run nix-darwin
+# cd ~/.config/nix-darwin
